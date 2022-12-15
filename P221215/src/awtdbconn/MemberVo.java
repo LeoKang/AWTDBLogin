73182170fd1b1ed8b8615ef8156ae040359e4453
Administrator <Administrator@User-2022ILFYWR>
2022-12-15 14:43:28 +0900

    Initial

---
+++ P221215/src/awtdbconn/MemberVo.java
@@ -0,0 +1,19 @@
+package awtdbconn;
+
+public class MemberVo {
+	private String id;
+	private String password;
+
+	public MemberVo(String id, String password) {
+		this.id = id;
+		this.password = password;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+}
